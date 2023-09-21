95817a584899b9cb7cd4582f2deb584a2ff2d68d
mohnishgorana1 <mohnishgorana1@gmail.com>
2023-09-21 18:47:01 +0530

    index page made

---
+++ src/components/index.js
@@ -0,0 +1,13 @@
+export { default as Header} from './Header'
+export { default as Navbar} from './Navbar'
+export { default as NavLinks} from './NavLinks'
+
+export { default as FormInput} from './FormInput'
+export { default as SubmitButton} from './SubmitButton'
+
+export { default as Hero} from './Hero'
+export { default as FeaturedProducts} from './FeaturedProducts'
+export { default as SectionTitle} from './SectionTitle'
+export { default as ProductsGrid} from './ProductsGrid'
+
+
