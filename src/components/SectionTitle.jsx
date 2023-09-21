73bc1f7d7c636d28106781e44810181a4699d6c1
mohnishgorana1 <mohnishgorana1@gmail.com>
2023-09-21 18:46:19 +0530

    section title component made

---
+++ src/components/SectionTitle.jsx
@@ -0,0 +1,10 @@
+
+function SectionTitle( {title} ) {
+  return (
+    <div className="border-b border-base-300 pb-5">
+        <h2 className="text-2xl sm:text-3xl font-medium tracking-wider capitalize">{title}</h2>
+    </div>
+  )
+}
+
+export default SectionTitle
