c04fd28c68be5f8bf59d017868878850b4c52abc
mohnishgorana1 <mohnishgorana1@gmail.com>
2023-09-22 15:41:43 +0530

    products page updateion

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -13,7 +13,7 @@ function Filters() {
   const pagination = meta.pagination;
 
   return (
-    <Form className="bg-base-200 rounded-md px-8 py-4 grid gap-x-4 gap-y-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 items-center">
+    <Form className="bg-base-200 rounded-lg px-8 py-4 grid gap-x-4 gap-y-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 items-center">
       {/* SEarch */}
       <FormInput
         type="search"
--- src/components/ProductsContainer.jsx
+++ src/components/ProductsContainer.jsx
@@ -1,9 +1,59 @@
-
+import { BsFillGridFill, BsList } from "react-icons/bs";
+import { ProductsGrid, ProductsList } from "../components";
+import { useState } from "react";
+import { useLoaderData } from "react-router-dom";
 
 function ProductsContainer() {
+  const { meta } = useLoaderData();
+  console.log(meta);
+
+  const totalProduct = meta.pagination.total;
+
+  const [layout, setLayout] = useState("grid");
+
+  const setActiveStyles = (pattern) => {
+    return `
+            text-xl btn btn-circle btn-sm ${
+              pattern === layout
+                ? "btn-primary text-primary-content"
+                : "btn-ghost text-based-content"
+            } 
+        `
+  };
+
   return (
-    <div>ProductsContainer</div>
-  )
+    <>
+      {/* {HEADER} */}
+      <div className="flex justify-between mt-8 border-b border-base-300 pb-5 ">
+        <h4 className="font-medium text-md">
+          {totalProduct} product{totalProduct > 1 && "s"}
+        </h4>
+
+        <div className="flex gap-x-2">
+          <button
+            type="button"
+            onClick={() => setLayout("grid")}
+            className={setActiveStyles("grid")}
+          >
+            <BsFillGridFill />
+          </button>
+
+          <button
+            type="button"
+            onClick={() => setLayout("list")}
+            className={setActiveStyles("list")}
+          >
+            <BsList />
+          </button>
+        </div>
+      </div>
+      {
+        totalProduct === 0 ? ( <h5 className="text-2xl mt-16">Sorry, no products matched...</h5>) 
+        : 
+        layout === "grid" ?  <ProductsGrid /> : <ProductsList />
+      }
+    </>
+  );
 }
 
-export default ProductsContainer
+export default ProductsContainer;
